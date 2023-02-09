4aa9a346b1ead1b9779737456a5ceae3f8c45e0d
Daniel Xin <danielxin@microsoft.com>
2023-02-10 00:56:27 +0800

    add downward api update delay test

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+﻿FROM cloudunder/java-runtime:openjdk8
+WORKDIR /downwardapi-test
+COPY ./target/downwardapi_test-1.0-SNAPSHOT-jar-with-dependencies.jar ./
+
+## install JRE
+#ENV JAVA_HOME=/usr/lib/jre
+#ENV PATH=${PATH}:${JAVA_HOME}/bin
+#RUN apt-get update \
+#    && apt-get install -y wget \
+#    && wget -nv -O jre.tar.gz "https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u352-b08/OpenJDK8U-jre_x64_linux_hotspot_8u352b08.tar.gz" \
+#    && mkdir ${JAVA_HOME} \
+#    && tar -xf jre.tar.gz -C $JAVA_HOME --strip-components 1 --no-same-owner \
+#    && rm jre.tar.gz
+
+ENTRYPOINT ["java", "-jar", "downwardapi_test-1.0-SNAPSHOT-jar-with-dependencies.jar"]
--- src/main/java/com/microsoft/danielxin/Main.java
+++ src/main/java/com/microsoft/danielxin/Main.java
@@ -3,22 +3,57 @@ package com.microsoft.danielxin;
 import io.fabric8.kubernetes.api.model.PodBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
-
-import java.util.Date;
+import java.io.File;
+import java.util.Scanner;
 
 public class Main {
 
 
     public static void main(String[] args) {
-        Date date = new Date();
         KubernetesClient k8s = new KubernetesClientBuilder().build();
+        File file = new File("/etc/downwardapi/annotations");
         try {
-             currentTime = date.toInstant();
-            k8s.pods().inNamespace("default").withName("my-pod").edit(p -> new PodBuilder(p).editMetadata()
-                    .addToAnnotations("currentTime", )
-                    .endMetadata()
-                    .build());
-        }
+            while (true) {
+                // 1 update annotation
+                System.out.println("Start update annotation [modifytime] ...");
+                long modifyTime = System.currentTimeMillis();
+                k8s.pods().inNamespace("default").withName("downwardapi-test").edit(p -> new PodBuilder(p).editMetadata()
+                        .addToAnnotations("modifytime", String.valueOf(modifyTime))
+                        .endMetadata()
+                        .build());
+                System.out.println("Update pod [downwardapi-test] annotation [modifytime] to [" + modifyTime + "].");
 
+                // 2 check if the annotation file is updated
+                System.out.println("Start read annotation from /etc/downwardapi/annotations ...");
+                while (true) {
+                    if (file.exists() && !file.isDirectory()) {
+                        Scanner scanner = new Scanner(file);
+                        String annotation = null;
+                        while (scanner.hasNext()) {
+                            annotation = scanner.next();
+                            if (annotation.startsWith("modifytime")) break;
+                        }
+                        if (annotation.startsWith("modifytime")) {
+                            long time = Long.parseLong(annotation.substring(annotation.indexOf('=') + 2, annotation.length() - 1));
+                            if (time == modifyTime) {
+                                long currentTime = System.currentTimeMillis();
+                                System.out.println("Get annotation: [" + annotation + "] Current time: [" + currentTime + "].");
+                                System.out.println("Annotation [modifytime] update to date, cost: [" + (currentTime - modifyTime) + "ms].");
+                                break;
+                            }
+                        }
+                        scanner.close();
+                    }
+                }
+
+                // 3 sleep 10 seconds
+                Thread.sleep(10 * 1000);
+            }
+        } catch (Exception e) {
+            System.out.println("Update annotation meet exception!");
+            System.out.println(e);
+        } finally {
+
+        }
     }
 }
